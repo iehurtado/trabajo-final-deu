397f378ee64fb50eda533e06daec34a446cce64a
Ignacio Hurtado <39395913+iehurtado@users.noreply.github.com>
2026-05-12 17:51:30 -0300

    ajusto menu de usuario

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,18 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { NgOptimizedImage } from '@angular/common';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faCog, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { NgbCollapseModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgbCollapseModule, NgbDropdownModule, RouterLink, NgOptimizedImage],
+  imports: [RouterOutlet, NgbCollapseModule, NgbDropdownModule, RouterLink, NgOptimizedImage, FaIconComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  protected readonly faRightFromBracket = faRightFromBracket;
   protected isNavbarCollapsed = signal(true);
 
   protected user = signal({
